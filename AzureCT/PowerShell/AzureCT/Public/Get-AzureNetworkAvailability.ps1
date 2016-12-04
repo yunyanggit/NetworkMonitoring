048a731c6716e3761d0ae9f0b8a29cf478f73950
yunyanggit <yunyanggit@users.noreply.github.com>
2016-12-04 16:58:20 -0600

    Update Get-AzureNetworkAvailability.ps1

--- AzureCT/PowerShell/AzureCT/Public/Get-AzureNetworkAvailability.ps1
+++ AzureCT/PowerShell/AzureCT/Public/Get-AzureNetworkAvailability.ps1
@@ -1,22 +1,6 @@
 
-
-
-    .LINK
-        https://github.com/tracsman/AzureCT
-
-    .LINK
-        Show-Results
-
-    .LINK
-        Clear-History
-
-    .NOTES
-        A specially built IIS server running in Azure is required for this cmdlet to work properly.
-        More information can be found at https://github.com/tracsman/AzureCT
-
-    #>
-
-    # Evaluate and Set input parameters
+ function Get-AzureVPN-Monitoring {
+    
     [cmdletBinding()]
     Param(
        [Parameter(ValueFromPipeline=$true,
@@ -24,9 +8,9 @@
                   HelpMessage='Enter IP Address of Remote Azure VM')]
        [ipaddress]$RemoteHost,
        [int]$DurationMinutes=1,
-       [int]$TimeoutSeconds=5
+       [int]$TimeoutSeconds=5,
+       [int]$AlertMin = 3
     )
-
     # Initialize
     $FilePath = $env:TEMP
     $HeaderFileName = "$FilePath\AvailabilityHeader.xml"
@@ -46,28 +30,28 @@
     [int]$WrapWidth = $Host.UI.RawUI.BufferSize.Width - 5
     $JobID = [System.Guid]::NewGuid().toString()
     $ErrorFlag = $false
-
+    [int]$AlertCount=0
+    [int]$HealthyCount=0
+ 
+   
     # Check for Header File
     If ((Test-Path $HeaderFileName) -eq $false) {
         [string]$JobHeaderFile = "<?xml version=`"1.0`"?><Jobs version=`"$script:XMLSchemaVersion`"><Job><ID/><StartTime/><EndTime/><Target/><TimeoutSeconds/><CallCount/><SuccessRate/><JobMin/><JobMax/><JobMedian/><ReferenceTrace/></Job></Jobs>"
         $JobHeaderFile | Out-File -FilePath $HeaderFileName -Encoding ascii}
-
     # Check for Detail File
     If ((Test-Path $DetailFileName) -eq $false) {
         [string]$JobDetailFile = "<?xml version=`"1.0`"?><JobRecords version=`"$script:XMLSchemaVersion`"><JobRecord><JobID/><CallID/><TimeStamp/><Return/><Display/><Valid/><Duration/><Tag/></JobRecord></JobRecords>"
         $JobDetailFile | Out-File -FilePath $DetailFileName -Encoding ascii}
-
     # Check for Trace File
     If ((Test-Path $TraceFileName) -eq $false) {
         [string]$TraceFile = "<?xml version=`"1.0`"?><TraceRecords version=`"$script:XMLSchemaVersion`"><TraceRecord><JobID/><CallID/><TimeStamp/><HopID/><Address/><TripTime/></TraceRecord></TraceRecords>"
         $TraceFile | Out-File -FilePath $TraceFileName -Encoding ascii}
-    
+   
     # Load Files and Get Ready for new run
     # Pull current Header and Detail xml files
     [xml]$JobHeaderFile = Get-Content $HeaderFileName
     [xml]$JobDetailFile = Get-Content $DetailFileName
     [xml]$TraceFile = Get-Content $TraceFileName
-
     # Create new Job Header xml node (in local file)
     $JobStart = Get-Date -Format 's'
     $JobHeader = ""
@@ -78,18 +62,15 @@
     $JobHeader.TimeoutSeconds = [string]$TimeoutSeconds
     $JobHeaderFile.Jobs.AppendChild($JobHeader) | Out-Null
     $JobHeaderFile.Save($HeaderFileName)
-
     # Job Loop, duration as defined by user input
     Try {
         Write-Host
         Write-Host "Starting Avilability test to $RemoteHost..." -ForegroundColor Cyan
         Write-Host
-
         # Run an initial call to load ARP tables and IIS caches along the call path
         Try {
             $WebCall = (Invoke-WebRequest -Uri http://$RemoteHost/WebTest.aspx -TimeoutSec 2)}
         Catch {}
-
         $CallArray = @()
         $TraceArray = @()
         Do {
@@ -98,7 +79,6 @@
             $ErrorType = "None"
             $CallDisplay = "n" # Null, this should never show
             $CallDisplayDescription = "Null" # This should never show
-
             # Call WebTest.aspx
              ###########################################
             #  The following line is the magic, it is   #
@@ -106,17 +86,15 @@
             #  $WebCall holds the results of the test.  #
             #  $CallDuration is the call duration in ms.#
              ###########################################
-
             Try {
                 $TraceArray += Start-Job -ScriptBlock {Get-IPTrace -RemoteHost $args[0] -JobID $args[1] -CallID $args[2]} -Name 'AzureCT.Tracing' -ArgumentList $RemoteHost, $JobID, $CallCount
                 $CallDuration = Measure-Command {$WebCall = (Invoke-WebRequest -Uri http://$RemoteHost/WebTest.aspx -TimeoutSec $TimeoutSeconds)}
-
                 # Pull server data from the test
                 $ServerTime = ($WebCall.AllElements | ? {$_.tagName -eq 'HEAD'}).innerText
                 $Result = ($WebCall.AllElements | ? {$_.tagName -eq 'BODY'}).innerText
             }
             Catch {
-                if ($error[0].Exception.Status -eq "Timeout") {$ErrorType = "Timeout"} 
+                if ($error[0].Exception.Status -eq "Timeout") {$ErrorType = "Timeout"}
                 Else {$ErrorType = "Other"} # Other Error, probably 404
                 $WebCall = ""
                 $CallDuration = ""
@@ -126,12 +104,11 @@
             }
             # Validate Server Return
             $Valid = [bool]($Result.Trim() -eq '1.0')
-    
-            If ($Valid) {$CallDisplay="!"; $CallDisplayDescription="Valid Call Response"} 
+   
+            If ($Valid) {$CallDisplay="!"; $CallDisplayDescription="Valid Call Response"}
             Elseif ($ErrorType -eq "None") {$CallDisplay="*"; $CallDisplayDescription="Bad Data Returned"; $Result = "Page Title: " + (($WebCall.AllElements | ? {$_.tagName -eq 'TITLE'}).innerText)}
             Elseif ($ErrorType -eq "Timeout") {$CallDisplay="."; $CallDisplayDescription="Timeout"}
             Else {$CallDisplay="*"; $CallDisplayDescription="Call Response Error"}
-
             # Do we need to keep the associated trace?
             # Note: the Tag field is matched at the end of this script
             #       and the trace data for those called tagged "true"
@@ -158,15 +135,17 @@
             Else {
                 $Tagged = $false
             }
-
             # Update Counters
-            If ($Valid) {$JobGood+=1} Else {$JobBad+=1}
+            If ($Valid) {$JobGood+=1; $AlertCount = 0; $HealthyCount +=1} Else {$JobBad+=1; $HealthyCount =0; $AlertCount+=1}
             [decimal]$SuccessRate = $JobGood/$CallCount*100
             $SuccessRate = "{0:N2}" -f $SuccessRate
             If ($CallDuration.TotalMilliseconds -lt $JobMin) {$JobMin = $CallDuration.TotalMilliseconds}
             If ($CallDuration.TotalMilliseconds -gt $JobMax) {$JobMax = $CallDuration.TotalMilliseconds}
             If ($Valid) {$CallArray += $CallDuration.TotalMilliseconds}
-
+            # Send out alert for VPN outage
+             If (-not $AlertFlag -and $AlertCount -gt $AlertMin ) {$AlertFlag = $true; write-host "Email VPN connection disconnect alert"}
+             If ( $AlertFlag -and $HealthyCount -gt $AlertMin ) {$AlertFlag = $false; write-host "Email VPN connection back to healthy"}
+  
             # Create Job Details xml
             $JobDetail=""
             $JobDetail = (@($JobDetailFile.JobRecords.JobRecord)[0]).Clone()
@@ -178,13 +157,11 @@
             $JobDetail.Valid = [string]$Valid
             $JobDetail.Duration = [string]$CallDuration.TotalMilliseconds
             $JobDetail.Tag = [string]$Tagged
-
             # Log new results xml to local file
             $JobDetailFile.JobRecords.AppendChild($JobDetail) | Out-Null
             $JobDetailFile.Save($DetailFileName)
-
             # Log summary results to local file
-            ForEach($Node in $JobHeaderFile.Jobs.Job) { 
+            ForEach($Node in $JobHeaderFile.Jobs.Job) {
                 If ($Node.ID -eq $JobID) {
                     $UpdatedNode = $Node
                     $UpdatedNode.CallCount = [string]$CallCount
@@ -195,11 +172,11 @@
                     $JobHeaderFile.Jobs.ReplaceChild($UpdatedNode, $Node) | Out-Null
                     }}
             $JobHeaderFile.Save($HeaderFileName)
-    
+   
             # Write output
             Write-Host $CallDisplay -NoNewline
             If ($CallCount%$WrapWidth -eq 0) {Write-Host}
-    
+   
             # Decide how long to sleep
             $SleepTime = 10000 - ((Get-Date) - [datetime]$CallTime).TotalMilliseconds
             If ($SleepTime -lt 0) {$SleepTime = 0}
@@ -207,12 +184,9 @@
         } # Do End
         While (([datetime]$JobStart + $RunDuration) -gt (Get-Date))
     } # Try End
-
     Finally {
-
         # Calculate end of job stats
         $JobEnd = Get-Date -Format 's'
-
         # Get Median Call Latency
         $CallArray = $CallArray | Sort
         If ($CallArray.count -eq 0) {
@@ -223,13 +197,11 @@
         }
         Else {
             $JobMedian = ($CallArray[$CallArray.Count/2],$CallArray[$CallArray.count/2-1] | Measure -Average).Average
-        }  
-
+        } 
         # Write summary host output
         Write-Host
         Write-Host
         Write-Host "Statistics for $RemoteHost"
-
         Write-Host "    Calls: Sent = " -NoNewline
         Write-Host $CallCount -NoNewline
         Write-Host ", Received = " -NoNewline
@@ -238,7 +210,6 @@
         Write-Host $SuccessRate"%" -NoNewline -ForegroundColor Green
         Write-Host "), Lost = " -NoNewline
         Write-Host $JobBad -ForegroundColor Red
-
         Write-Host "Approximate round trip times in milli-seconds:"
         Write-Host "    Minimum = " -NoNewline
         Write-Host $JobMin -NoNewline
@@ -247,9 +218,7 @@
         Write-Host "ms, Median = " -NoNewline
         Write-Host $JobMedian -NoNewline
         Write-Host "ms"
-
-        Write-Host 
-
+        Write-Host
         # Update Job Header xml in local file
         ForEach($node in $JobHeaderFile.Jobs.Job) {
             If ($node.ID -eq $JobID) {
@@ -260,13 +229,12 @@
                 }
         }
         $JobHeaderFile.Save($HeaderFileName)
-
         # Wait for traces to finish
         While ((Get-Job -Name "AzureCT.Tracing" | Where State -eq 'Running').Count -gt 0) {
             Sleep -Seconds 2
             Write-Host "Waiting for Trace Route jobs to finish..."
         }
-        
+       
         # Build the Trace File for Upload
         ForEach ($Node in $JobDetailFile.JobRecords.JobRecord) {
             If ($Node.JobID -eq $JobID -and $Node.Tag -eq "True") {
@@ -285,21 +253,17 @@
                 } # End ForEach $TraceRow
             } # End If
         } # End ForEach $Node
-
         ForEach ($Job in (Get-Job)) {
            Remove-Job $Job
         }
-
         # Upload Header, Detail, and Trace xml to server
         $uri = "http://$RemoteHost/Upload.aspx"
         $contentType = "multipart/form-data"
         Try {
             $header = @{FileID = "Header"}
             $HeaderUploadResponse = (Invoke-WebRequest -Uri $uri -ContentType $contentType -Method Post -Body $JobHeaderFile.OuterXml -Headers $header -TimeoutSec 10).Content.Trim()
-
             $header = @{FileID = "Detail"}
             $DetailUploadResponse = (Invoke-WebRequest -Uri $uri -ContentType $contentType -Method Post -Body $JobDetailFile.OuterXml -Headers $header -TimeoutSec 15).Content.Trim()
-
             $header = @{FileID = "Trace"}
             $TraceUploadResponse = (Invoke-WebRequest -Uri $uri -ContentType $contentType -Method Post -Body $TraceFile.OuterXml -Headers $header -TimeoutSec 10).Content.Trim()
         }
@@ -308,21 +272,18 @@
             $DetailUploadResponse = "Bad"
             $TraceUploadResponse = "Bad"
         }
-
         Write-Host
         If ($HeaderUploadResponse -eq "Good" -and $DetailUploadResponse -eq "Good" -and $TraceUploadResponse -eq "Good") {
-            Write-Host "Data uploaded to remote server sucessfully"
-
+            Write-Host "Data uploaded to remote server successfully”
             # Spawn local web browser showing report details from server
             Write-Host "Launching browser to http://$RemoteHost"
             Start-Process -FilePath "http://$RemoteHost"
-
             # Close and Clean Up
-            # Clean up local files 
+            # Clean up local files
             Remove-Item $HeaderFileName
             Remove-Item $DetailFileName
             Remove-Item $TraceFileName
-        } 
+        }
         Else {
             Write-Warning "Data upload to remote server failed."
             Write-Warning "Please check to ensure the remote server has all the files required to run this tool."
@@ -331,4 +292,3 @@
         Write-Host
     } # Finally End
 } # Function End
-
